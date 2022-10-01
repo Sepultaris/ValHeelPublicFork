24813f60f2a6e9d84ba0d9019deec2d2332883b0
Sepultaris <49662778+Sepultaris@users.noreply.github.com>
2022-09-30 23:04:23 -0400

    Fixed wrong operands on T9 pet device gear rating chances

--- Source/ACE.Server/Factories/LootGenerationFactory_PetDevice.cs
+++ Source/ACE.Server/Factories/LootGenerationFactory_PetDevice.cs
@@ -77,17 +77,17 @@ namespace ACE.Server.Factories
 
                 // add rng ratings to pet device
                 // linear or biased?
-                if (ratingChance > ThreadSafeRandom.Next(1.0f, 1.0f))
+                if (ratingChance <= ThreadSafeRandom.Next(1.0f, 1.0f))
                     petDevice.GearDamage = GeneratePetDeviceRating(tier);
-                if (ratingChance > ThreadSafeRandom.Next(1.0f, 1.0f))
+                if (ratingChance <= ThreadSafeRandom.Next(1.0f, 1.0f))
                     petDevice.GearDamageResist = GeneratePetDeviceRating(tier);
-                if (ratingChance > ThreadSafeRandom.Next(1.0f, 1.0f))
+                if (ratingChance <= ThreadSafeRandom.Next(1.0f, 1.0f))
                     petDevice.GearCritDamage = GeneratePetDeviceRating(tier);
-                if (ratingChance > ThreadSafeRandom.Next(1.0f, 1.0f))
+                if (ratingChance <= ThreadSafeRandom.Next(1.0f, 1.0f))
                     petDevice.GearCritDamageResist = GeneratePetDeviceRating(tier);
-                if (ratingChance > ThreadSafeRandom.Next(1.0f, 1.0f))
+                if (ratingChance <= ThreadSafeRandom.Next(1.0f, 1.0f))
                     petDevice.GearCrit = GeneratePetDeviceRating(tier);
-                if (ratingChance > ThreadSafeRandom.Next(1.0f, 1.0f))
+                if (ratingChance <= ThreadSafeRandom.Next(1.0f, 1.0f))
                     petDevice.GearCritResist = GeneratePetDeviceRating(tier);
 
                 petDevice.ItemWorkmanship = WorkmanshipChance.Roll(tier);
